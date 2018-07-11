4445f493a752b1589c7b99caedb1aa9304f31ca0
pierre-yves lajoie <pierre-yves.lajoie@polymtl.ca>
2018-07-11 13:21:24 -0400

    Squared Mahalanobis distance computation

--- src/robust_multirobot_slam.cpp
+++ src/robust_multirobot_slam.cpp
@@ -14,7 +14,26 @@ namespace robust_multirobot_slam {
     }
 
     double computeSquaredMahalanobisDistance(const geometry_msgs::PoseWithCovariance& pose) {
-        
+        // Extraction of the covariance matrix
+        Eigen::Matrix<double, 6, 6> covariance_matrix;
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 6; j++) {
+                covariance_matrix(i,j) = pose.covariance[i*6+j];
+            }
+        }
+
+        // Extraction of the pose vector
+        Eigen::VectorXd pose_vector(6);
+        pose_vector(0) = pose.pose.position.x;
+        pose_vector(1) = pose.pose.position.y;
+        pose_vector(2) = pose.pose.position.z;
+        pose_vector(3) = pose.pose.orientation.x;
+        pose_vector(4) = pose.pose.orientation.y;
+        pose_vector(5) = pose.pose.orientation.z;
+
+        // Computation of the squared Mahalanobis distance
+        double distance = pose_vector.transpose() * covariance_matrix * pose_vector;
+        return distance;
     }
 
 }
